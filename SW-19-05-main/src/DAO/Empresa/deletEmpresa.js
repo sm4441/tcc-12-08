d3357471d19348ff7dd50242f5c0166519100971
sm4441 <samuelribeirodossantossilva@gmail.com>
2025-10-10 07:36:23 -0300

    Update deletEmpresa.js
    talvez funcione

--- SW-19-05-main/src/DAO/Empresa/deletEmpresa.js
+++ SW-19-05-main/src/DAO/Empresa/deletEmpresa.js
@@ -2,7 +2,7 @@ const {conexao} = require('../conexao.js')
 
 async function deletarEmpresa(id){
 
-    let sql = 'DELETE FROM tbl_empresa WHERE id = ?'
+    let sql = 'DELETE FROM tbl_vaga WHERE id_vaga = ?'
     
     sql = `DELETE FROM tbl_empresa WHERE id = ?`
     const conn = await conexao()
@@ -20,3 +20,4 @@ async function deletarEmpresa(id){
 
 
 module.exports = {deletarEmpresa}
+
